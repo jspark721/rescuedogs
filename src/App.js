aba29a2dc234d87c46eb260159319c0ce9748f0b
jspark721 <jspark721@gmail.com>
2020-04-15 15:23:34 -0700

    add npm tools - prettier & eslint

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-const Dog = ({name, breed, gender }) => {
+const Dog = ({
+    name,
+    breed,
+    gender
+}) => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, name),
         React.createElement("h2", {}, breed),
         React.createElement("h2", {}, gender),
-    ])
-}
-
+    ]);
+};
 
 const App = () => {
     return React.createElement(
-        "div",
-        { id: "navbar" },
+        "div", {
+            id: "navbar"
+        },
         //whatever children of the div you want to pass in
         [
             React.createElement("h1", {}, "Rescue Dogs"),
-            React.createElement(Dog, { 
-                name: "Harvey", 
+            React.createElement(Dog, {
+                name: "Harvey",
                 breed: "Shih Tzu",
-                gender: "Male"
+                gender: "Male",
             }),
-            React.createElement(Dog, { 
-                name: "Mochi", 
+            React.createElement(Dog, {
+                name: "Mochi",
                 breed: "Maltese",
-                gender: "Female"
+                gender: "Female",
             }),
-            React.createElement(Dog, { 
-                name: "Rusty", 
+            React.createElement(Dog, {
+                name: "Rusty",
                 breed: "Toy Poodle",
-                gender: "Male"
+                gender: "Male",
             }),
         ]
     );
 };
 
-ReactDOM.render(
-    React.createElement(App), 
-    document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
