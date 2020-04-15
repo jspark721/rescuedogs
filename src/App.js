aec20929ba07864f036cfe2acf40914cb48b4a8b
jspark721 <jspark721@gmail.com>
2020-04-15 15:41:03 -0700

    install parcel bundler

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-const Dog = ({
-    name,
-    breed,
-    gender
-}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, breed),
-        React.createElement("h2", {}, gender),
-    ]);
+import React from "react";
+import { render } from "react-dom";
+
+const Dog = ({ name, breed, gender }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, breed),
+    React.createElement("h2", {}, gender),
+  ]);
 };
 
 const App = () => {
-    return React.createElement(
-        "div", {
-            id: "navbar"
-        },
-        //whatever children of the div you want to pass in
-        [
-            React.createElement("h1", {}, "Rescue Dogs"),
-            React.createElement(Dog, {
-                name: "Harvey",
-                breed: "Shih Tzu",
-                gender: "Male",
-            }),
-            React.createElement(Dog, {
-                name: "Mochi",
-                breed: "Maltese",
-                gender: "Female",
-            }),
-            React.createElement(Dog, {
-                name: "Rusty",
-                breed: "Toy Poodle",
-                gender: "Male",
-            }),
-        ]
-    );
+  return React.createElement(
+    "div",
+    { id: "navbar" },
+    //whatever children of the div you want to pass in
+    [
+      React.createElement("h1", {}, "Rescue Dogs"),
+      React.createElement(Dog, {
+        name: "Harvey",
+        breed: "Shih Tzu",
+        gender: "Male",
+      }),
+      React.createElement(Dog, {
+        name: "Mochi",
+        breed: "Maltese",
+        gender: "Female",
+      }),
+      React.createElement(Dog, {
+        name: "Rusty",
+        breed: "Toy Poodle",
+        gender: "Male",
+      }),
+    ]
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
