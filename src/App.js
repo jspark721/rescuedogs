05c9f2bbf3b720c36c58f675f7dcee2a1c21c792
jspark721 <jspark721@gmail.com>
2020-04-15 20:46:45 -0700

    eslint add jsx

--- src/App.js
+++ src/App.js
@@ -3,29 +3,38 @@ import { render } from "react-dom";
 import Dog from "./Dog";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    { id: "navbar" },
-    //whatever children of the div you want to pass in
-    [
-      React.createElement("h1", {}, "Rescue Dogs"),
-      React.createElement(Dog, {
-        name: "Harvey",
-        breed: "Shih Tzu",
-        gender: "Male",
-      }),
-      React.createElement(Dog, {
-        name: "Mochi",
-        breed: "Maltese",
-        gender: "Female",
-      }),
-      React.createElement(Dog, {
-        name: "Rusty",
-        breed: "Toy Poodle",
-        gender: "Male",
-      }),
-    ]
+  // return React.createElement(
+  //   "div",
+  //   { id: "navbar" },
+  //   //whatever children of the div you want to pass in
+  //   [
+  //     React.createElement("h1", {}, "Rescue Dogs"),
+  //     React.createElement(Dog, {
+  //       name: "Harvey",
+  //       breed: "Shih Tzu",
+  //       gender: "Male",
+  //     }),
+  //     React.createElement(Dog, {
+  //       name: "Mochi",
+  //       breed: "Maltese",
+  //       gender: "Female",
+  //     }),
+  //     React.createElement(Dog, {
+  //       name: "Rusty",
+  //       breed: "Toy Poodle",
+  //       gender: "Male",
+  //     }),
+  //   ]
+  // );
+
+  return (
+    <div>
+      <h1 id="logo">Rescue Dogs</h1>
+      <Dog name="Harvey" breed="Shih Tzu" gender="Male" />
+      <Dog name="Mochi" breed="Maltese" gender="Female" />
+      <Dog name="Rusty" breed="Toy Poodle" gender="Male" />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Dog.js
+++ src/Dog.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 export default function Dog({ name, breed, gender }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, breed),
-    React.createElement("h2", {}, gender),
-  ]);
+  return (
+    <div>
+      <h1>{name.toUpperCase()}</h1>
+      <h2>{breed}</h2>
+      <h2>{gender}</h2>
+    </div>
+  );
 }
