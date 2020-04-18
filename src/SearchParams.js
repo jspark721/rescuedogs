5ce4e2f1cbcf66d53e33c9facfc927ffe7c381fd
jspark721 <jspark721@gmail.com>
2020-04-17 21:23:46 -0700

    async & await function, and add fallback mock API for testing

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -7,6 +7,18 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  //async guarantees to return a promise that will resolve whenever the function completes 
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    })
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -20,7 +32,10 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit= {(e) => {
+        e.preventDefault();
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location
           <input id="location" value={location} placeholder="Location" onChange={event => setLocation(event.target.value)}/>
