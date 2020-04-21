496633c1ccefb14c4ea085dfc0773692cb26ca1e
jspark721 <jspark721@gmail.com>
2020-04-20 23:41:10 -0700

    install reach router and fix prettier extension

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import 'babel-polyfill';
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1 id="logo">Rescue Dogs</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id"/>
+        </Router>
       </div>
     </React.StrictMode>
   );
